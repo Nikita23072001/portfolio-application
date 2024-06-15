84308334bf7b078f4e9c04f03dc2044803c70623
unknown <nikita.gordynskii@gmail.com>
2024-06-15 17:11:04 +0200

    Added and started my first django site

--- portfolio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Project, Skill
-
-def portfolio(request):
-    projects = Project.objects.all()  # Get all projects
-    skills = Skill.objects.all()  # Get all skills
-    context = {'projects': projects, 'skills': skills}
-    return render(request, 'portfolio/portfolio.html', context)
--- urls.py
+++
@@ -1,8 +0,0 @@
-from django import urls
-from django.urls import path
-from .portfolio import views  # Assuming "portfolio" directory is at the same level
-
-
-urlpatterns = [
-    path('', views.portfolio, name='portfolio'),
-]
